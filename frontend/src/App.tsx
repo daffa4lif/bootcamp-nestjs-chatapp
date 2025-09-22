210d7f7928ef457d069662125c7fc297fbfe98e1
Daffa Alif Murtaja <daffa4lif@gmail.com>
2025-09-22 10:59:37 +0700

    set theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,18 @@
+import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark'
+  }
+})
+
 const App = () => {
-  return <div className="">Hellow world</div>
+  return <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
+    <Container>
+      <div className="">Hellow world</div>
+    </Container>
+  </ThemeProvider>
 }
 
 export default App;
