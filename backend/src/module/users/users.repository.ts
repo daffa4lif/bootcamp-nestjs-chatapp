958aa1a74c48b17f0322b14766d434b42136000e
Daffa Alif Murtaja <daffa4lif@gmail.com>
2025-09-22 20:21:01 +0700

    rename class

--- backend/src/module/users/users.repository.ts
+++ backend/src/module/users/users.repository.ts
@@ -1,14 +1,14 @@
 import { AbstractRepository } from 'src/common/database/abstract.repository';
-import { UserDocument } from './entities/user.schema';
+import { User } from './entities/user.entity';
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 @Injectable()
-export class UserRepository extends AbstractRepository<UserDocument> {
+export class UserRepository extends AbstractRepository<User> {
   protected readonly logger = new Logger(UserRepository.name);
 
-  constructor(@InjectModel(UserDocument.name) userModel: Model<UserDocument>) {
+  constructor(@InjectModel(User.name) userModel: Model<User>) {
     super(userModel);
   }
 }
