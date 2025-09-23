80f60cc9ea5b3c4a9fd29a1aebed9a9eeb986306
Daffa Alif Murtaja <daffa4lif@gmail.com>
2025-09-23 14:31:26 +0700

    create endpoint post user graphql

--- frontend/src/pages/auth/Register.tsx
+++ frontend/src/pages/auth/Register.tsx
@@ -1,9 +1,23 @@
 import { Button, Stack, TextField } from "@mui/material";
 import { useState } from "react";
+import { useCreateUser } from "../../hooks/useCreateUser";
 
 const RegisterPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const [createUser] = useCreateUser();
+
+  const submit = async (email: string, password: string) => {
+    await createUser({
+      variables: {
+        createUserInput: {
+          email,
+          password,
+        },
+      },
+    });
+  };
 
   return (
     <Stack
@@ -30,7 +44,9 @@ const RegisterPage = () => {
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      <Button>Register</Button>
+      <Button variant="contained" onClick={() => submit(email, password)}>
+        Register
+      </Button>
     </Stack>
   );
 };
