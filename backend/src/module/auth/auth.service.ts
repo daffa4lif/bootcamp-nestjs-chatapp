64fdb2b7d0e429b12ffd328e0189c32cbd875a83
Daffa Alif Murtaja <daffa4lif@gmail.com>
2025-09-24 14:20:11 +0700

    set expired at login

--- backend/src/module/auth/auth.service.ts
+++ backend/src/module/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '../users/entities/user.entity';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly configService: ConfigService) {}
+
+  async login(user: User, response: Response) {
+    const expires = new Date();
+    expires.setSeconds(
+      expires.getSeconds() +
+        Number(this.configService.getOrThrow('JWT_EXPIRED')),
+    );
+  }
+}
