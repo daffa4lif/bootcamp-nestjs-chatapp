eb18e7f6c40f1c49211921bee57f93f4274da9c8
Daffa Alif Murtaja <daffa4lif@gmail.com>
2025-09-25 11:13:11 +0700

    _id didapat dari current user auth

--- backend/src/module/users/users.resolver.ts
+++ backend/src/module/users/users.resolver.ts
@@ -3,6 +3,10 @@ import { UsersService } from './users.service';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
 import { User } from './entities/user.entity';
+import { CurrentUser } from '../auth/current-user.decorator';
+import type { TokenPayload } from '../auth/interface/token-payload.interface';
+import { UseGuards } from '@nestjs/common';
+import { GplAuthGuard } from '../auth/guards/gpl-auth.guard';
 
 @Resolver(() => User)
 export class UsersResolver {
@@ -23,9 +27,13 @@ export class UsersResolver {
     return this.usersService.findOne(_id);
   }
 
+  @UseGuards(GplAuthGuard)
   @Mutation(() => User)
-  updateUser(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
-    return this.usersService.update(updateUserInput._id, updateUserInput);
+  updateUser(
+    @Args('updateUserInput') updateUserInput: UpdateUserInput,
+    @CurrentUser() user: TokenPayload,
+  ) {
+    return this.usersService.update(user._id, updateUserInput);
   }
 
   @Mutation(() => User)
