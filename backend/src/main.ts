bf463accd585f0beba76a504e686122abd048b95
Daffa Alif Murtaja <daffa4lif@gmail.com>
2025-09-25 10:46:00 +0700

    add cookie parser to app

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
@@ -15,6 +16,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.useLogger(app.get(Logger));
+  app.use(cookieParser());
 
   await app.listen(configService.getOrThrow('PORT'));
 }
